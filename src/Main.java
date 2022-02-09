f70a36316decda1560274453aa46e844404a6185
erin-day-2 <daye4@mail.uc.edu>
2022-02-09 10:51:01 -0500

    finished

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,18 @@ public class Main
 
     public static void main(String[] args)
     {
-	// write your code here
+	boolean monthRange = false;
+    int birthMonth = 13;
+
+    if (birthMonth >=1 && birthMonth <=12)
+        {
+            monthRange = true;
+            System.out.println("Your birth month is:" + birthMonth);
+        }
+    else
+        {
+            monthRange = false;
+            System.out.println("You entered an incorrect month value:" + birthMonth);
+        }
     }
 }
